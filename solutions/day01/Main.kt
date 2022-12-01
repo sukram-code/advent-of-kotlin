2ef68334953b1f8895a41363d159d3766c4c8bfc
sukram-code <sukram.code@gmail.com>
2022-12-01 22:44:09 +0200

    day01 some cleanup

--- solutions/day01/Main.kt
+++ solutions/day01/Main.kt
@@ -7,28 +7,26 @@ fun main(args: Array<String>) {
     stopIfInvalid(args)
     val elves = inputLines("01", args[0])
         .toElves()
-        .sortedByDescending { it.sumOfCalories() }
-    println("Top 1 Elf has ${elves.take(1).sumOf { it.sumOfCalories() }} calories")
-    println("Top 3 Elves have ${elves.take(3).sumOf { it.sumOfCalories() }} calories")
+        .sortedByDescending { it.calories }
+    println("Top 1 Elf has ${elves[0].calories} calories")
+    println("Top 3 Elves have ${elves.take(3).sumOf { it.calories }} calories")
 }
 
-data class Elf(val calories: List<Int>) {
-    fun sumOfCalories() = calories.sum()
-}
+data class Elf(val calories: Int)
 
 fun List<String>.toElves(): List<Elf> {
     val elfs = mutableListOf<Elf>()
     var items = mutableListOf<Int>()
     for (line in this) {
         if (line.isEmpty()) {
-            elfs.add(Elf(items))
+            elfs.add(Elf(items.sum()))
             items = mutableListOf()
         } else {
             items.add(line.toInt())
         }
 
         if (indexOf(line) == this.size - 1) {
-            elfs.add(Elf(items))
+            elfs.add(Elf(items.sum()))
         }
     }
     return elfs
