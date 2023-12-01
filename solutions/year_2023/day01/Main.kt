e61c2d6f851ce960357294b76cad5ea037235b3f
sukram-code <sukram.code@gmail.com>
2023-12-01 19:54:34 +0200

    2023 day01 part2

--- solutions/year_2023/day01/Main.kt
+++ solutions/year_2023/day01/Main.kt
@@ -6,12 +6,47 @@ import common.stopIfInvalid
 
 fun main(args: Array<String>) {
     stopIfInvalid(args)
-    inputLines(year = 2023, dayId = "01", fileName = args[0])
-        .toNumbers()
+    val lines = inputLines(year = 2023, dayId = "01", fileName = args[0])
+    solvePartOne(lines)
+    solvePartTwo(lines)
+}
+
+fun solvePartOne(lines: List<String>) {
+    "Part 1:".printIt()
+    lines
+        .extractNumbers()
         .sum()
         .printIt()
 }
 
-fun List<String>.toNumbers(): List<Int> = map { string -> string.filter { it.isDigit() } }
-    .map { string -> "${string.first()}${string.last()}".toInt() }
-    .toList()
+fun solvePartTwo(lines: List<String>) {
+    "Part 2:".printIt()
+    lines
+        .wordsToDigits()
+        .extractNumbers()
+        .sum()
+        .printIt()
+}
+
+fun List<String>.extractNumbers(): List<Int> = map { string -> string.filter { it.isDigit() } }
+    .map { string -> if (string.isEmpty()) 0 else "${string.first()}${string.last()}".toInt() }
+
+fun List<String>.wordsToDigits(): List<String> = map { string -> string.replaceWordsToWordsWithDigits() }
+
+fun String.replaceWordsToWordsWithDigits(): String {
+    var result = this
+    digitsAsWords.keys.forEach { key -> result = result.replace(key, digitsAsWords[key] ?: key) }
+    return result
+}
+
+val digitsAsWords = mapOf(
+    "one" to "one1one",
+    "two" to "two2two",
+    "three" to "three3three",
+    "four" to "four4four",
+    "five" to "five5five",
+    "six" to "six6six",
+    "seven" to "seven7seven",
+    "eight" to "eight8eight",
+    "nine" to "nine9nine"
+)
